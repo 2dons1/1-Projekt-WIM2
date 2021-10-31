59dab2c5c579bc75142a4d6414051bb6b6b028db
dorian dons <dorian.doncevic@gmail.com>
2021-10-31 13:20:18 +0100

    Jos jedan testni commit

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const PORT = process.env.PORT || 80
 
 express()
   .get('/', (req, res) => res.send("Ovo je naslovna stranica projekta, testiranje"))
+  .get('/test', (req, res) => res.send("OVO JE TEST"))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
