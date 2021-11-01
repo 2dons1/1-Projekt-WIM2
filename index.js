66d6edc038f2aa83e64c73ada8ce2069a54c55f1
dorian dons <dorian.doncevic@gmail.com>
2021-11-01 12:10:58 +0100

    Views i ejs

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const PORT = process.env.PORT || 80
 const TOKEN =  process.env.TOKEN
 
 express()
+  .set("view engine", "ejs")
   .get('/', (req, res) => res.send("Ovo je naslovna stranica projekta, testiranje"))
   .get('/locationMap', function(req, res){
-    res.sendFile(path.join(__dirname, '/locationMap.html'));
+    // res.sendFile(path.join(__dirname, 'views/locationMap.ejs'));
+    res.render("locationMap");
   })
   .get('/locationText', function(req, res){
-    res.sendFile(path.join(__dirname, '/locationText.html'));
+    // res.sendFile(path.join(__dirname, 'views/locationText.ejs'));
+    res.render("locationText");
   })
   .get('/geolocation.js', function(req, res){
-    res.sendFile(path.join(__dirname, '/geolocation.js'));
+    res.sendFile(path.join(__dirname, 'views/geolocation.js'));
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
