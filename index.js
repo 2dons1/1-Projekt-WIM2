ba64c48ec2f46ce3bb0862bd0e300bf414b154a8
dorian dons <dorian.doncevic@gmail.com>
2021-11-01 11:07:36 +0100

    Dodana geolokacija - tekstualno

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ const PORT = process.env.PORT || 80
 express()
   .get('/', (req, res) => res.send("Ovo je naslovna stranica projekta, testiranje"))
   .get('/test', (req, res) => res.send("OVO JE TEST"))
+  .get('/locationText', function(request, response){
+    response.sendFile(path.join(__dirname, '/locationText.html'));
+  })
+  .get('/geolocation.js', function(request, response){
+    response.sendFile(path.join(__dirname, '/geolocation.js'));
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
