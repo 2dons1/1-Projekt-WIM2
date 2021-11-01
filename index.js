44e75b271e650de29d6347e87a3d6cea35c9f4f0
dorian dons <dorian.doncevic@gmail.com>
2021-11-01 13:12:48 +0100

    Markeri i info

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 80
-const TOKEN =  process.env.TOKEN
+var TOKEN =  process.env.TOKEN
 
 express()
   .set("view engine", "ejs")
   .get('/', (req, res) => res.send("Ovo je naslovna stranica projekta, testiranje"))
   .get('/locationMap', function(req, res){
-    // res.sendFile(path.join(__dirname, 'views/locationMap.ejs'));
-    res.render("locationMap");
+    res.render("locationMap", {token: TOKEN});
   })
   .get('/locationText', function(req, res){
-    // res.sendFile(path.join(__dirname, 'views/locationText.ejs'));
     res.render("locationText");
   })
   .get('/geolocation.js', function(req, res){
