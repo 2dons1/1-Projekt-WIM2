eaecdb47a6b5a5e74268fb5c1c5222343420004f
dorian dons <dorian.doncevic@gmail.com>
2021-11-01 17:32:45 +0100

    Dodana auth0, potrebno testirati

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 3000
-var TOKEN =  process.env.TOKEN
+const TOKEN =  process.env.TOKEN
+const baseURL = process.env.APP_URL || "http://localhost:3000"
+const { auth, requiresAuth } = require('express-openid-connect');
 
 express()
+  .use(
+    auth({
+      authRequired: false,
+      auth0Logout: true,
+      issuerBaseURL: process.env.ISSUER_BASE_URL,
+      baseURL: baseURL,
+      clientID: process.env.CLIENT_ID,
+      secret: process.env.SECRET,
+      idpLogout: true,
+    })
+  )
   .set("view engine", "ejs")
-  .get('/', (req, res) => res.send("Ovo je naslovna stranica projekta, testiranje"))
+  .get('/', function(req, res){
+    res.send(req.oidc.isAuthenticated() ? 'Logged In' : 'Logged Out');
+  })
   .get('/locationMap', function(req, res){
     res.render("locationMap", {token: TOKEN});
   })
@@ -15,5 +30,8 @@ express()
   })
   .get('/geolocation.js', function(req, res){
     res.sendFile(path.join(__dirname, 'views/geolocation.js'));
+  })
+  .get('/private', requiresAuth(), function(req, res){
+    res.send(JSON.stringify(req.oidc.user))
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
